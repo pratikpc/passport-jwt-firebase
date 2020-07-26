6ba0877f81ada7826978d3618bbe193c8e167b6f
Pratik Chowdhury <pratikc@live.co.uk>
2020-07-26 05:35:45 +0530

    Updated Sample with New Version Code

--- samples/lib/index.ts
+++ samples/lib/index.ts
@@ -2,34 +2,50 @@ import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import passport from "passport";
 import FirebaseStrategy, { TokenLoader } from "passport-jwt-firebase";
+import { ExtractJwt } from "passport-jwt";
 
 TokenLoader.Load();
 const app: Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-passport.use(FirebaseStrategy);
+passport.use(
+  new FirebaseStrategy(
+    // Optional
+    // By Default uses Bearer Token
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    // Optional
+    (user: any, done: any) => {
+      return done(null, user);
+    }
+  )
+);
 app.use(passport.initialize());
+app.use(passport.authenticate("jwt", { session: false }));
 
-app.use(passport.authenticate('jwt', { session: false }));
-
-app.use("/", passport.authenticate('jwt', { session: false }), (req: Request, res: Response) => {
-    if(req.isUnauthenticated())
-        return res.status(401);
+app.use(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req: Request, res: Response) => {
+    if (req.isUnauthenticated()) return res.status(401);
     const user = {
-        ...req.user!,
-        authenticated: req.isAuthenticated()
+      ...req.user!,
+      authenticated: req.isAuthenticated(),
     };
-    console.log(user);
     return res.json(user);
-});
+  }
+);
 
 const Port = Number(process.env.PORT || 8000);
-app.listen(Port, err => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log(`Server is listening on ${Port}`);
-    console.log(`To Query, send Bearer token to localhost:${Port}`);
-    console.log(`Generate Firebase Bearer Token at https://pratikpc.github.io/google-firebase-auth-token/`);
+app.listen(Port, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(`Server is listening on ${Port}`);
+  console.log(`To Query, send Bearer token to localhost:${Port}`);
+  console.log(
+    `Generate Firebase Bearer Token at https://pratikpc.github.io/google-firebase-auth-token/`
+  );
 });
