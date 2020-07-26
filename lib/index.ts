4a733c70a67c714cae548cfcff08b498e8b481f7
Pratik Chowdhury <pratikc@live.co.uk>
2020-07-26 05:37:04 +0530

    Updated to new version

--- lib/index.ts
+++ lib/index.ts
@@ -1,32 +1,50 @@
-import { User } from "@firebase/auth-types";
-import { Strategy as JWTstrategy, ExtractJwt } from "passport-jwt";
+import {
+  Strategy as JWTstrategy,
+  ExtractJwt,
+  StrategyOptions,
+  VerifyCallbackWithRequest,
+  VerifyCallback,
+  VerifiedCallback,
+} from "passport-jwt";
 import { Request } from "express";
 import TokenLoader from "./TokenLoader";
 
-function FirebaseSecretOrKeyProvider(_: Request, rawJwtToken: string, done: any) {
+export default class FirebaseStrategy extends JWTstrategy {
+  public static FirebaseSecretOrKeyProvider(
+    _: Request,
+    rawJwtToken: string,
+    done: any
+  ) {
     try {
-        const header64 = rawJwtToken.split('.')[0];
-        const header = JSON.parse(Buffer.from(header64, 'base64').toString('ascii'));
-        const secret = TokenLoader.Get(String(header.kid));
-        return done(null, secret);
+      const header64 = rawJwtToken.split(".")[0];
+      const header = JSON.parse(
+        Buffer.from(header64, "base64").toString("ascii")
+      );
+      const secret = TokenLoader.Get(String(header.kid));
+      return done(null, secret);
     } catch (err) {
-        return done(err, null);
+      return done(err, null);
     }
+  }
+  public static DefaultVerfierCallBack(user: any, done: VerifiedCallback) {
+    return done(null, user);
+  }
+  constructor(
+    options: StrategyOptions = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    verify:
+      | VerifyCallback
+      | VerifyCallbackWithRequest = FirebaseStrategy.DefaultVerfierCallBack
+  ) {
+    super(
+      {
+        ...options,
+        secretOrKeyProvider: FirebaseStrategy.FirebaseSecretOrKeyProvider,
+      },
+      verify
+    );
+  }
 }
 
-//This verifies that the token sent by the user is valid
-const FirebaseStrategy = new JWTstrategy({
-    secretOrKeyProvider: FirebaseSecretOrKeyProvider,
-    //we expect the user to send the token as a query parameter with the name 'secret_token'
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-}, async (token: User, done) => {
-    try {
-        //Pass the user details to the next middleware
-        return done(null, token);
-    } catch (error) {
-        done(error, null);
-    }
-});
-
-export default FirebaseStrategy;
 export { TokenLoader };
