b4f53c82436ba3fdcb819d084dcc29b2a1580990
Pratik Chowdhury <pratikc@live.co.uk>
2020-07-26 05:35:27 +0530

    Use Node-Fetch instead of axios

--- lib/TokenLoader.ts
+++ lib/TokenLoader.ts
@@ -1,10 +1,17 @@
-import axios from "axios";
+import fetch from "node-fetch";
 
+type KVSPair = {
+  [key: string]: string;
+};
 export default class TokenLoader {
-  public static tokens: any = {};
+  public static tokens: KVSPair = {};
 
   public static async LoadOnce() {
-    this.tokens = (await axios.get("https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")).data;
+    this.tokens = await (
+      await fetch(
+        "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com"
+      )
+    ).json();
   }
   public static async Load(interval: number = 2 * 60 * 60 * 1000) {
     await this.LoadOnce();
@@ -17,8 +24,7 @@ export default class TokenLoader {
     const token = this.tokens[key];
     // Have the keys changed maybe?
     // Time to Reload and Check
-    if (token == null)
-      this.LoadOnce();
+    if (token == null) this.LoadOnce();
     return String(token);
   }
-};
+}
